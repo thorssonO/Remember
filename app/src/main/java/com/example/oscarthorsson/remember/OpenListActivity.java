dc18c2b43771bbd844e3a3df47db3dad517cd946
ClaraG <clara_3232@hotmail.com>
2017-11-23 08:40:30 +0100

    här är det! :)

--- app/src/main/java/com/example/oscarthorsson/remember/OpenListActivity.java
+++ app/src/main/java/com/example/oscarthorsson/remember/OpenListActivity.java
@@ -2,6 +2,10 @@ package com.example.oscarthorsson.remember;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class OpenListActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class OpenListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_list);
+        showList();
+    }
+
+    public void showList(){
+
+        FakeReminderStore sara = FakeReminderStore.getInstance();
+        List<ReminderList> test = sara.getReminders();
+        ListView listView = findViewById(R.id.test2);
+        ArrayAdapter adapter = new ArrayAdapter<ReminderList>(this,android.R.layout.simple_expandable_list_item_1,test);
+        listView.setAdapter(adapter);
     }
 }
